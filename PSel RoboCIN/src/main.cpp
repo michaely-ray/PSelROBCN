148ed305dc2724ce8e340a395b5f916fc6f0b4dd
Michaely Raiany <michaely.raiany@gmail.com>
2022-09-07 14:47:43 -0300

    Encoder and Speed code

--- PSel RoboCIN/src/main.cpp
+++ PSel RoboCIN/src/main.cpp
@@ -1,32 +1,45 @@
 #include "mbed.h"
-
 // Blinking rate in milliseconds
-#define BLINKING_RATE     500
+#define BLINKING_RATE     2000ms
 
-//include an interruption with button 
-InterruptIn button(BUTTON1);
-DigitalOut led(LED2);
-int counter=0;
-static BufferedSerial serial_port(USBTX, USBRX, 230400);
+InterruptIn sensor(PB_1);
+// PwmOut pwm(PA_8);
+DigitalOut test(PA_8); //Using a LED indicator
 
-FileHandle *mbed::mbed_override_console(int fd)
-{
-   return &serial_port;
-}
+float period = 0.001f;
+Timer t;
+uint8_t counter_pulse = 0;
+uint64_t current_p = 0, last_t = 0;
+float f = 0;
 
-void controller1();
+void frequenceCounterISR();
 
-int main()
-{   
-    button.rise(&controller1); //The Led will torn on or off and the counter adds one when the button trigger 
+int main() {
 
-    while (true) {
-        printf("Led is now %d\n", led.read());
-        printf("contador de clicks %d\n", counter);
-        ThisThread::sleep_for(BLINKING_RATE);
+    t.start();
+    sensor.rise(NULL);
+    while(1){
+        if (current_p != 0){
+            f = 1000000 * ((float)(counter_pulse)/current_p); // Calculate frequency Hz
+        }
+        else{
+        f = 0;
+    }
+        // Reset pulse parameters
+        counter_pulse = 0;                          
+        current_p = 0;                  
+        last_t = t.elapsed_time().count();
+        printf("%d\n", int(last_t));        
+        sensor.rise (&frequenceCounterISR); // start interrupt
+        printf("%d\n", int(f*30)); //speed in RPM
+        ThisThread::sleep_for(500ms);  // Wait new measure
     }   
 }
-void controller1() {
-    led=!led;
-    counter++;
+
+void frequenceCounterISR(){
+    counter_pulse++;
+    printf("%d\n", int(counter_pulse));                  // Counter of pulse
+    current_p += t.elapsed_time().count() - last_t;     // Size of pulse
+    last_t = t.elapsed_time().count();                  // Reset timer     
+    test = !test;
 }
