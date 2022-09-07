8d1a00d1eb3088ec899395923cd801485dcec912
Michaely Raiany <michaely.raiany@gmail.com>
2022-09-07 13:40:43 -0300

    First Interruption

--- PSel RoboCIN/src/main.cpp
+++ PSel RoboCIN/src/main.cpp
@@ -1,12 +1,12 @@
 #include "mbed.h"
 
 // Blinking rate in milliseconds
-#define BLINKING_RATE     2000
+#define BLINKING_RATE     500
 
-
-DigitalOut led(LED1); //Include LED as digital output
+//include an interruption with button 
+InterruptIn button(BUTTON1);
+DigitalOut led(LED2);
 int counter=0;
-
 static BufferedSerial serial_port(USBTX, USBRX, 230400);
 
 FileHandle *mbed::mbed_override_console(int fd)
@@ -14,16 +14,19 @@ FileHandle *mbed::mbed_override_console(int fd)
    return &serial_port;
 }
 
+void controller1();
 
 int main()
-{
+{   
+    button.rise(&controller1); //The Led will torn on or off and the counter adds one when the button trigger 
+
     while (true) {
-        led=!led; //Reading and change the status led
-        if(led.read()){
-            counter++;
-            printf("Blink! LED is now %d\n", counter);
-        } 
-    
-        ThisThread::sleep_for(BLINKING_RATE); //Delay
+        printf("Led is now %d\n", led.read());
+        printf("contador de clicks %d\n", counter);
+        ThisThread::sleep_for(BLINKING_RATE);
     }   
+}
+void controller1() {
+    led=!led;
+    counter++;
 }
